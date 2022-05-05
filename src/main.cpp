349a4a1c7d9aa5b76b0ef9c0b0f37f660ff385fc
r2road <studior2road@gmail.com>
2022-05-06 07:46:36 +0900

    for exe

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,21 @@
 	#include <vld.h>
 #endif
 
-#include "r2cm/r2cm_Director.h"
 #include "r2bix/r2utility_WindowUtil.h"
 
-#include "DevelopmentMenu.h"
+#define DEVELOPMENT_2048MINI 0
+
+#if defined( DEVELOPMENT_2048MINI ) && ( DEVELOPMENT_2048MINI == 1 )
+
+	#include "r2cm/r2cm_Director.h"
+	#include "DevelopmentMenu.h"
+
+#else
+
+	#include "r2bix/r2base_Director.h"
+	#include "p2048mini/p2048mini_GameScene.h"
+
+#endif
 
 int main()
 {
@@ -27,15 +38,21 @@ int main()
 	r2utility::MoveWindow( 0, 0 );
 
 	//
-	// Setup
+	// Setup & Run
 	//
+#if defined( DEVELOPMENT_2048MINI ) && ( DEVELOPMENT_2048MINI == 1 )
+
 	r2cm::Director director;
 	director.Setup( DevelopmentMenu::Create( director ) );
-
-	//
-	// Process
-	//
 	director.Run();
+
+#else
+
+	r2base::Director director;
+	director.Setup( p2048mini::GameScene::Create( director ) );
+	director.Run();
+
+#endif
 
 	return 0;
 }

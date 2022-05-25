70da97cfd0d70c2aed8e9c65fd8c9591b58d7489
r2road <studior2road@gmail.com>
2022-05-25 21:00:52 +0900

    remove class : r2bix - PivotComponent

--- src/r2bix/r2node_PivotNode.cpp
+++ src/r2bix/r2node_PivotNode.cpp
@@ -1,7 +1,7 @@
 #include "r2node_PivotNode.h"
 
 #include "r2base_Director.h"
-#include "r2component_PivotComponent.h"
+#include "r2component_CustomTextureComponent.h"
 #include "r2component_TextureRenderComponent.h"
 
 namespace r2node
@@ -12,9 +12,11 @@ namespace r2node
 		if( ret )
 		{
 			r2component::TextureRenderComponent* texture_render_component = ret->AddComponent<r2component::TextureRenderComponent>();
-			r2component::PivotComponent* pivot_component = ret->AddComponent<r2component::PivotComponent>();
+			r2component::CustomTextureComponent* custome_texture_component = ret->AddComponent<r2component::CustomTextureComponent>();
+			custome_texture_component->GetTexture()->Reset( 1u, 1u, '+' );
+			custome_texture_component->GetTexture()->FillColor( 1u, 1u, r2base::eForegroundColor::FG_Red | r2base::eBackgroundColor::BG_White );
 
-			texture_render_component->SetTexture( pivot_component->GetTexture() );
+			texture_render_component->SetTexture( custome_texture_component->GetTexture() );
 		}
 
 		return ret;

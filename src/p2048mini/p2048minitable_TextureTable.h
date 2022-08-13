295969a616ad96dd817f70b76dc77680b7b4d15d
r2road <studior2road@gmail.com>
2022-08-13 23:59:04 +0900

    cleanup

--- src/p2048mini/p2048minitable_TextureTable.h
+++ src/p2048mini/p2048minitable_TextureTable.h
@@ -1,16 +1,7 @@
 #pragma once
 
-#include <memory>
-#include <string>
-#include <unordered_map>
-
 #include "r2cm/r2cm_Singleton.h"
 #include "r2bix/r2base_TextureTableBase.h"
-
-namespace r2render
-{
-	class Texture;
-}
 
 namespace p2048minitable
 {

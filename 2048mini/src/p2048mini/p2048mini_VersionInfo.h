195fe1866147b5829e8115684eb2962b8145511e
r2road <studior2road@gmail.com>
2022-08-21 19:23:35 +0900

    ...

--- 2048mini/src/p2048mini/p2048mini_VersionInfo.h
+++ 2048mini/src/p2048mini/p2048mini_VersionInfo.h
@@ -16,7 +16,7 @@ namespace p2048mini
 		const char VersionNumber_2 = '0';
 		const char VersionNumber_3 = '5';
 
-		const char String4Version[19] = { 'p', '2', '0', '4', '8', 'm', 'i', 'n', 'i', ' ', ':', ' ', 'v', VersionNumber_1, '.', VersionNumber_2, '.', VersionNumber_3, '\0' };
+		const char String4Version[18] = { '2', '0', '4', '8', 'm', 'i', 'n', 'i', ' ', ':', ' ', 'v', VersionNumber_1, '.', VersionNumber_2, '.', VersionNumber_3, '\0' };
 
 	} VersionInfo;
 }
